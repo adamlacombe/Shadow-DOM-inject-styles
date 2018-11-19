08338dce5f9d8b735ce8e9c67b2a6112de12a13b
Florian Comte <comte.florian@gmail.com>
2018-11-19 23:24:28 +0100

    TSLint friendly

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-export function injectStyles(shadowRootElement: HTMLElement, insertBeforeSelector: string, styles: string) {
-	let root              = shadowRootElement.shadowRoot;
-	let styleAlreadyAdded = false;
-	let currentStyleTags  = Array.from(root.querySelectorAll('style'));
-	currentStyleTags.forEach((element: HTMLStyleElement, index) => {
-		if (element.innerHTML == styles) {
-			styleAlreadyAdded = true;
-		}
-		if ((currentStyleTags.length - 1) == index && styleAlreadyAdded == false) {
-			let newStyleTag       = document.createElement('style');
-			newStyleTag.innerHTML = styles;
-			root.insertBefore(newStyleTag, root.querySelector(insertBeforeSelector));
-		}
-	});
+export function injectStyles(
+  shadowRootElement: HTMLElement,
+  insertBeforeSelector: string,
+  styles: string
+) {
+  const root = shadowRootElement.shadowRoot;
+  let styleAlreadyAdded = false;
+  const currentStyleTags = Array.from(root.querySelectorAll('style'));
+  currentStyleTags.forEach((element: HTMLStyleElement, index) => {
+    if (element.innerHTML === styles) {
+      styleAlreadyAdded = true;
+    }
+    if (currentStyleTags.length - 1 === index && styleAlreadyAdded == false) {
+      const newStyleTag = document.createElement('style');
+      newStyleTag.innerHTML = styles;
+      root.insertBefore(newStyleTag, root.querySelector(insertBeforeSelector));
+    }
+  });
 }
